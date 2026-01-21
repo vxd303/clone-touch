d1987f96cca166654dbfadd277a5fdc27acf95d9
vxd303 <vanxinhdep303@gmail.com>
2026-01-20 23:08:09 -0800

    Enhance OpenCV header inclusion for compatibility
    Added conditional includes for OpenCV headers to support different layouts.

--- zxtouch-binary/TemplateMatch.h
+++ zxtouch-binary/TemplateMatch.h
@@ -1,11 +1,30 @@
+// NOTE:
+// The OpenCV iOS framework bundled in this repo can have different header
+// layouts depending on how it was built/copied. Some distributions expose:
+//   opencv2.framework/Headers/opencv2/opencv.hpp
+// others expose a flat layout under Headers/.
+//
+// To keep builds working across both layouts (local + CI), we:
+//   1) Add -I.../opencv2.framework/Headers (and /Headers/opencv2) in Makefile.
+//   2) Use tolerant includes here.
+
 #ifndef TEMPLATE_MATCH_H
 #define TEMPLATE_MATCH_H
-#endif 
 
 #ifdef __cplusplus
-#undef NO
-#undef YES
-#import <opencv2/opencv.hpp>
+  #undef NO
+  #undef YES
+  #if __has_include(<opencv2/opencv.hpp>)
+    #include <opencv2/opencv.hpp>
+  #elif __has_include(<opencv.hpp>)
+    #include <opencv.hpp>
+  #elif __has_include(<opencv2.hpp>)
+    #include <opencv2.hpp>
+  #elif __has_include(<opencv2.h>)
+    #include <opencv2.h>
+  #else
+    #error "OpenCV headers not found. Check frameworks/opencv2.framework and header search paths."
+  #endif
 #endif
 
 
@@ -20,7 +39,13 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreMedia/CoreMedia.h>
-#import <imgcodecs/ios.h>
+#if __has_include(<opencv2/imgcodecs/ios.h>)
+  #import <opencv2/imgcodecs/ios.h>
+#elif __has_include(<imgcodecs/ios.h>)
+  #import <imgcodecs/ios.h>
+#else
+  // Some OpenCV builds omit iOS helpers; keep optional.
+#endif
 
 @interface TemplateMatch : NSObject
 
@@ -37,3 +62,5 @@
 - (void)setMaxTryTimes:(int)mtt;
 
 @end
+
+#endif /* TEMPLATE_MATCH_H */
