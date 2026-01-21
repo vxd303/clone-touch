a7ca93d3e464869e3768b94b6110ff1e8fd464f7
vxd303 <vanxinhdep303@gmail.com>
2026-01-21 07:02:27 -0800

    Create Image.h

---
+++ zxtouch-binary/Image.h
@@ -0,0 +1,24 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+#undef NO
+#undef YES
+#import <opencv.hpp>
+#endif
+
+using namespace cv;
+using namespace std;
+
+
+@interface Image : NSObject
+{
+
+}
+
+
+@end
+
+#endif
